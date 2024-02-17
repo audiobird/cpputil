ac026d35e312d06bee74a64c9701293402b32e7f
audiobird <cantrellhepner@gmail.com>
2024-02-17 10:26:52 -0800

    .

--- util/bitset.hh
+++ util/bitset.hh
@@ -4,15 +4,14 @@
 #include <cstdint>
 #include <type_traits>
 
+// similar to std::bitset but uses smallest possible integer
+
 template<uint8_t bit_cnt>
 class bitset {
-	static_assert(bit_cnt <= 32);
+	static_assert(bit_cnt <= 32, "Consider using std::bitset");
 	using type = std::conditional_t<bit_cnt <= 8, uint8_t, std::conditional_t<bit_cnt <= 16, uint16_t, uint32_t>>;
-	static constexpr type mask = static_cast<type>(-1);
-	type state;
-	static constexpr type getMask(const uint8_t pos) {
-		return 1u << pos;
-	}
+	static constexpr type mask = (1u << bit_cnt) - 1u;
+	type state = 0u;
 
 public:
 	class reference {
@@ -23,28 +22,33 @@ public:
 
 	public:
 		constexpr reference(const reference &) = default;
+
 		constexpr reference(type &val, const uint8_t pos)
 			: val{val}
-			, mask{getMask(pos)} {
+			, mask{static_cast<type>(1u << pos)} {
 		}
-		constexpr ~reference() {
-		}
+
 		constexpr reference &operator=(const auto x) = delete;
+
 		constexpr reference &operator=(const bool x) {
 			val = x ? val | mask : val & ~mask;
 			return *this;
 		}
+
 		constexpr reference &operator=(const reference &x) {
 			val = x ? val | mask : val & ~mask;
 			return *this;
 		}
+
 		constexpr operator bool() const {
 			return val & mask;
 		}
+
 		constexpr reference &flip() {
 			val ^= mask;
 			return *this;
 		}
+
 		constexpr bool operator~() const {
 			return !(val & mask);
 		}
@@ -76,7 +80,6 @@ public:
 	constexpr bool none() const {
 		return state == 0;
 	}
-
 	constexpr uint8_t size() const {
 		return bit_cnt;
 	}
@@ -85,26 +88,32 @@ public:
 		state = mask;
 		return *this;
 	}
+
 	constexpr bitset &set(const uint8_t pos, const bool value = true) {
 		reference{state, pos} = value;
 		return *this;
 	}
+
 	constexpr bitset &reset() {
 		state = 0;
 		return *this;
 	}
+
 	constexpr bitset &reset(const uint8_t pos) {
 		reference{state, pos} = false;
 		return *this;
 	}
+
 	constexpr bitset &flip() {
 		state ^= mask;
 		return *this;
 	}
+
 	constexpr bitset &flip(const uint8_t pos) {
 		reference{state, pos}.flip();
 		return *this;
 	}
+
 	constexpr auto to_uintx() const {
 		return state;
 	}

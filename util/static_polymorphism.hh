b26dda3df46c6419e2abb640dc1486a6620b535a
audiobird <cantrell@madronemusic.com>
2024-10-25 11:42:44 -0700

    requires

--- util/static_polymorphism.hh
+++ util/static_polymorphism.hh
@@ -6,14 +6,15 @@
 
 // creates a data buffer that can hold the largest type passed in
 template<typename Base, typename Derived, typename... Derived_>
+requires(are_base_of<Base, Derived, Derived_...>())
 class StaticPolymorphicContainer {
-	std::array<uint8_t, sizeofLargestType<Derived, Derived_...>()> buffer;
-	static_assert(are_base_of<Base, Derived, Derived_...>());
+	std::array<uint8_t, sizeof_largest_type<Derived, Derived_...>()> buffer;
 
 public:
 	// Instantiates the given type into the static buffer
 	// TODO: better name?
 	template<typename T, typename... Ts>
+	requires(is_same_as<T, Derived, Derived_...>())
 	void transform(Ts &&...args) {
 		static_assert(is_same_as<T, Derived, Derived_...>());
 		static_cast<void>(new (buffer.data()) T{args...});
